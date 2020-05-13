3d6d155b1db011567f596aeb0ff230163d35ef53
YYuki220 <207x122x@stu.kobe-u.ac.jp>
2020-05-13 21:54:24 +0900

    20

--- W02/vec3.js
+++ W02/vec3.js
@@ -65,45 +65,7 @@ class Vec3
     }
 
 
+
+
 }
 
-
-
-
-/*//Constructor
-Vec3 = function(x,y,z)
-{
-    this.x=x;
-    this.y=y;
-    this.z=z;
-}
-
-//Add method
-Vec3.prototype.add=function(v)
-{
-    this.x=x;
-    this.y=y;
-    this.z=z;
-    return this;
-}
-
-Vec3.prototype.sum=function()
-{
-    return this.x+this.y+this.z;
-}
-/*
-Vec3.prototype.min=funcion()
-{
-    return (Math.min(this.x,this.y,this.z));
-}
-
-Vec3.prototype.max=funcion()
-{
-    return (Math.max(this.x,this.y,this.z));
-}
-
-Vec3.prototype.mid=funcion()
-{
-    return (Math.median(this.x,this.y,this.z));
-}
-*/
